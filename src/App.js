fd68d18833ebafcd13abb3cea73e3274ae499080
Poorva Lalawat <poorvalalawat@Poorva-Lalawat-I157s-MacBook-Pro.local>
2022-04-08 15:10:23 +0530

    Created React Table and Routing part

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {BasicTable} from './components/BasicTable'
+import {Form} from './components/Form'
+import {Routes, Route} from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element ={<BasicTable/>}/>
+      <Route path='form' element={<Form/>}/>
+    </Routes>
+    // <div className="App">
+    //   <BasicTable/>
+    // </div>
   );
 }
 
